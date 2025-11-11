536147a47f02396ae2a0081acfbb28c59bf49707
Javier <jviermuzWORK@gmail.com>
2025-11-11 16:57:23 +0100

    Correccion

--- 1_redis/ingesta_alvaro.py
+++ 1_redis/ingesta_alvaro.py
@@ -61,7 +61,7 @@ try:
     # Usamos el cliente .json() de la librería de redis-py
     
     # La clave sigue el patrón: info:id
-    KEY_PREFIX = "hospital:madrid:empleado"
+    KEY_PREFIX = "empleados"
     
     count = 0
     for emp_id, data in empleados_data.items():
