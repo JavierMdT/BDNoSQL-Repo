a865c74949609d80c0f56aca93e77d7d501f7bf0
Javier <jviermuzWORK@gmail.com>
2025-11-16 13:49:15 +0100

    Toml hecho

--- 1_redis/cloud/ingesta.py
+++ 1_redis/cloud/ingesta.py
@@ -2,14 +2,21 @@
 import redis 
 from redis.sentinel import Sentinel
 from typing import Tuple, Any
+import tomli
+
+# Cargamos el usuario y contraseña
+with open("access.toml", "rb") as f:
+    access_dict = tomli.load(f)
+user = access_dict["user"]
+password = access_dict["pass"]
 
 
 r = redis.Redis(
     host='redis-18235.c339.eu-west-3-1.ec2.cloud.redislabs.com',
     port=18235,
     decode_responses=True,
-    username="default",
-    password="YH1GGgeVfom6CLUGL1ao5EtHK5keOMW5",
+    username=user,
+    password=password,
 )
 
 print(f"[INFO] Correcta conexion a la base de datos.")
