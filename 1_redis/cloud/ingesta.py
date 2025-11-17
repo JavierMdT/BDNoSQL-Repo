f47a9204df597f7938cd8a90876a876647391a1c
Javier <jviermuzWORK@gmail.com>
2025-11-17 11:34:07 +0100

    Terminado GEO hospitales CLOUD

--- 1_redis/cloud/ingesta.py
+++ 1_redis/cloud/ingesta.py
@@ -28,7 +28,36 @@ r.flushall()
 
 # * Datos geograficos hospitales *
 '''
-Tipo de dato: GEO --> (nombre, geohash)
+Tipo: GEO --> (nombre, geohash)
+Clave: "Hospitales"
 Ejemplo:
-    - Lugares : [(Torre_Eiffel, u4dru), ...]
+    - Hospitales: [(nombre1, u4dru), ...]
 '''
+
+# Hospitales
+id_hospitales = {
+    "111": "Clinica del Sol Naciente",
+    "222": "Hospital Sierra Vista",
+    "333": "Centro Medico Aurora",
+    "444": "Sanatorio Nido de Paz",
+    "555": "Hospital General Fenix"
+}
+
+# Coordenadas
+coordenadas = [
+    (-3.7038, 40.4168),  # Centro (Puerta del Sol / Plaza Mayor)
+    (-3.6190, 40.4851),  # Norte (Barrio de Fuencarral/Las Tablas)
+    (-3.7547, 40.3846),  # Oeste (Casa de Campo / Alcorcón limítrofe)
+    (-3.6300, 40.3705),  # Sur (Barrio de Vallecas / Villaverde)
+    (-3.6670, 40.4300)   # Este (Barrio de Salamanca / Ventas)
+]
+
+# Añadimos los datos
+datos = []
+for partial_id in range(5):
+    datos.append(coordenadas[partial_id][0]) # Longitud
+    datos.append(coordenadas[partial_id][1]) # Latitud 
+    datos.append(id_hospitales[str(partial_id+1)*3]) # Nombre del hospital
+
+r.geoadd(name="hospitales",
+         values=datos)
